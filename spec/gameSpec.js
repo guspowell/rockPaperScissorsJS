2886122d0ec37b205ad482669fc5791c883290ad
guspowell <augustuspowell@me.com>
2015-03-24 22:31:01 +0000

    can determine the winning, losing and drawing hands; can determine the outcome of the game

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -6,7 +6,6 @@ describe("game", function() {
   beforeEach(function() {
     game = new Game();
     player = new Player('gus');
-    spyOn(game, "computerChoice").and.returnValue('rock')
   });
 
   describe('defaults', function() {
@@ -33,8 +32,67 @@ describe("game", function() {
   describe('computer choice', function() {
 
     it('the computer should be able to choose from the options', function() {
-      expect(game.computerChoice()).toEqual('rock')
-      console.log(game.computerChoice());
+      spyOn(game, "randomChoice").and.callFake(function() {
+        game.computerChoice = 'rock'
+      });
+      game.randomChoice();
+      expect(game.computerChoice).toEqual('rock');
+    });
+
+  });
+
+  describe('knowing the outcomes', function() {
+
+    beforeEach(function() {
+      spyOn(game, "randomChoice").and.callFake(function() {
+        game.computerChoice = 'rock'
+      });
+    });
+
+    it('should know how a player wins', function() {
+      game.randomChoice();
+      player.pickOption('paper');
+      expect(game.isWin(player)).toEqual(true)
+    });
+
+    it('should know a player can draw', function() {
+      game.randomChoice();
+      player.pickOption('rock');
+      expect(game.isDraw(player)).toEqual(true)
+    });
+
+    it('should know how a can lose', function() {
+      game.randomChoice();
+      player.pickOption('scissors');
+      expect(game.isLoss(player)).toEqual(true)
+    });
+
+  });
+
+  describe('determining the outcome', function() {
+
+    beforeEach(function() {
+      spyOn(game, "randomChoice").and.callFake(function() {
+        game.computerChoice = 'rock'
+      });
+    });
+
+    it('should say win if the player has a winning hand', function() {
+      game.randomChoice();
+      player.pickOption('paper');
+      expect(game.outcome(player)).toEqual('win');
+    });
+
+    it('should say lose if the player has a losing hand', function() {
+      game.randomChoice();
+      player.pickOption('scissors');
+      expect(game.outcome(player)).toEqual('lose');
+    });
+
+    it('should say lose if the player has a drawing hand', function() {
+      game.randomChoice();
+      player.pickOption('rock');
+      expect(game.outcome(player)).toEqual('draw');
     });
 
   });
--- src/game.js
+++ src/game.js
@@ -10,14 +10,43 @@ Player.prototype.pickOption = function (option) {
 
 
 var Game = function() {
-  this.choices = ['rock', 'paper', 'scissors']
-  this.players = []
+  this.choices = ['rock', 'paper', 'scissors'];
+  this.players = [];
+  this.computerChoice = null
 };
 
 Game.prototype.addPlayer = function (player) {
   this.players.push(player)
 };
 
-Game.prototype.computerChoice = function () {
-  return this.choices[Math.floor(Math.random() * this.choices.length)];
+Game.prototype.randomChoice = function () {
+  this.computerChoice = this.choices[Math.floor(Math.random() * this.choices.length)];
+};
+
+Game.prototype.isWin = function (player) {
+  if (
+    player.choice === 'rock' && this.computerChoice === 'scissors' ||
+    player.choice === 'scissors' && this.computerChoice === 'paper' ||
+    player.choice === 'paper' && this.computerChoice === 'rock'
+  ) {
+    return true
+  }
+};
+
+Game.prototype.isDraw = function (player) {
+  if (player.choice === this.computerChoice) { return true; }
+};
+
+Game.prototype.isLoss = function (player) {
+  if (!this.isWin(player) && !this.isDraw(player)) { return true; }
+};
+
+Game.prototype.outcome = function (player) {
+  if (this.isWin(player)) {
+    return 'win';
+  } else if (this.isLoss(player)) {
+    return 'lose';
+  } else {
+    return 'draw';
+  }
 };
